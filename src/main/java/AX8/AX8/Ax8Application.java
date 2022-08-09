dbfde70dd44dca58947e05ed0ca224f715f9c73a
harolp0810 <harold.p2329@gmail.com>
2022-08-08 23:37:08 -0500

    Initial commit

---
+++ src/main/java/AX8/AX8/Ax8Application.java
@@ -0,0 +1,13 @@
+package AX8.AX8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ax8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ax8Application.class, args);
+	}
+
+}
---
+++ src/test/java/AX8/AX8/Ax8ApplicationTests.java
@@ -0,0 +1,13 @@
+package AX8.AX8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ax8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

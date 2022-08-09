1ea248a8b2e36782a5c72cd0f77277fef09d467c
harolp0810 <harold.p2329@gmail.com>
2022-08-09 00:01:50 -0500

    Carpetas

--- src/main/java/AX8/AX8/aplicacion/dto/usuarioDTO.java
+++ src/main/java/AX8/AX8/aplicacion/dto/usuarioDTO.java
@@ -1,4 +1,27 @@
 package AX8.AX8.aplicacion.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+
+/*
+Etiquetas lombook
+
+Funcionamiento:
+
+@NoArgsConstructor: Genera un Constructor sin parametros
+@AllArgsConstructor: Genera un constructor con un paramentro para cada campo de su clase
+@Gettery/o @Setterpara permitir que lombok genere el getter/setter predeterminado automáticamente.
+
+
+ */
+
+
 public class usuarioDTO {
+    private String usuario;
+    private String clave;
 }
